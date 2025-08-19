454576c258d2ceb48cc330991fedc80e7e9cef14
sanzida-mim <mim.3ingenious@gmail.com>
2025-08-20 01:50:53 +0600

    Started working on a Movie App using React and TailwindCSS.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className="pattern" />
+        <div className="wrapper">
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle!</h1>
+        </div>
+    </main>
   )
 }
 
