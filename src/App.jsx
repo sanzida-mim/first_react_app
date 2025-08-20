c2623cff5c36f61a403b7772ef2f891fb6bb520e
sanzida-mim <mim.3ingenious@gmail.com>
2025-08-21 02:05:18 +0600

    Integrated TMDB API and rendered tranding movie list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Search from "./components/Search"
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [movieList, setmovieList] = useState([]);
+  const [isloading, setisloading] = useState(false);
+
+  const fetchMovies = async () => {
+    setisloading(true);
+    setErrorMsg('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+
+      if(data.Response === 'False') {
+        setErrorMsg(data.Error || 'Failed to fetch movies');
+        setmovieList([]);
+        return;
+      }
+
+      setmovieList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMsg('Error fetching movies. Please try again later.');
+    } finally {
+      setisloading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -12,11 +61,25 @@ const App = () => {
           <header>
             <img src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className="text-gradient">Movies</span> You"ll Enjoy Without the Hassle!</h1>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <section className="all-movies">
+            <h2>All Movies</h2>
 
-          <h1 className="text-white">{searchTerm}</h1>
+            {isloading ? (
+              <p className="text-white">Loading...</p>
+            ) : errorMsg ? (
+              <p className="text-red-500">{errorMsg}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <p className="text-white">{movie.title}</p>
+                ))}
+              </ul>
+            )}
+          </section>
         </div>
     </main>
   )
