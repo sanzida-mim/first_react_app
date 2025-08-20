1c41064efdd97d2650e52fec74f98ddabf6bae6d
sanzida-mim <mim.3ingenious@gmail.com>
2025-08-20 22:34:08 +0600

    Worked with first React component 'Search'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from "react"
+import Search from "./components/Search"
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern" />
+
         <div className="wrapper">
-          <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle!</h1>
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>Find <span className="text-gradient">Movies</span> You"ll Enjoy Without the Hassle!</h1>
+          </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+          <h1 className="text-white">{searchTerm}</h1>
         </div>
     </main>
   )
